6407049b2b628768911bec836124dd2324ad0691
Maxim Sergeev <themaximsergeev@gmail.com>
2020-12-02 09:39:05 +0200

    Function containing Axios post request to backend email controller.

---
+++ front/src/services/emailAPI.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const baseUrl = '/api/email'
+
+export default function sendConfirmationEmail(confirmation, options, email) {
+  return axios
+    .post(baseUrl, { confirmation, options, email })
+    .then(res => res.data)
+}
