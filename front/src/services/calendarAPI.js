5266506f2d4d93770ceb5ec10f55e5527c21f938
Maxim Sergeev <themaximsergeev@gmail.com>
2020-12-02 09:38:29 +0200

    Function containing Axios post request to backend calendar controller.

---
+++ front/src/services/calendarAPI.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const baseUrl = '/api/calendar'
+
+export default function addEventToCalendar(entry, order, options) {
+  return axios
+      .post(baseUrl, { entry, order, options })
+      .then(res => res.data)
+}
