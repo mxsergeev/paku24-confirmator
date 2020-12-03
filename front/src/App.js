913f477a6e99b0fbfa1b6b3bbd9c4e863c25cdeb
Maxim Sergeev <themaximsergeev@gmail.com>
2020-12-03 10:39:00 +0200

    Added simple login functionality.

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,65 @@
-import React, { useState } from 'react'
-import { ErrorBoundary } from 'react-error-boundary'
+import React, { useState, useEffect, useCallback } from 'react'
+import Toast from 'light-toast'
 import Convert from './components/Convert'
+import loginService from './services/login'
+import { ErrorBoundary } from 'react-error-boundary'
+import loginServiсe from './services/login'
+import Logo from './components/Logo'
+import './styles/container.css'
 
-
-function ErrorFallback({ error, resetErrorBoundary }) {
+function ErrorFallback({ error }) {
   return (
     <div role="alert">
       <p>Something went wrong:</p>
       <pre style={{color: 'red'}}>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
   )
 }
-
+ 
 function App() {
-  const [text, setText] = useState('')
+  const [logged, setLogged] = useState(false)
 
+  const tryToLogin = useCallback(() => {
+    loginService
+      .checkPass()
+      .then((res) => {
+        const { isCorrect, message, throttleTime } = res
+        console.log(isCorrect)
+
+        if (message) {
+          Toast.fail(`${message} Try again after ${throttleTime / 1000} seconds.`, throttleTime-400)
+          setTimeout(() => {
+            tryToLogin()
+          }, throttleTime)
+          return 
+        }
+        if (!isCorrect) {
+          tryToLogin()
+        }
+  
+        setLogged(isCorrect)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    const storedPass = loginServiсe.getStoredPass()
+    if (storedPass) {
+      return setLogged(true)
+    }
+
+    if (!logged) {
+      tryToLogin()
+    }
+
+  }, [logged, tryToLogin])
+  
   return (
-    <div>
-      <ErrorBoundary
-        FallbackComponent={ErrorFallback}
-        onReset={() => {
-          setText('')
-        }}
-        resetKeys={[text]}
-      >
-        <Convert text={text} setText={setText}/>
+    <div className="container">
+      <Logo />
+
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        {logged ? <Convert /> : null}
       </ErrorBoundary>
     
     </div>
