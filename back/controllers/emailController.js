08c5734d56ca2693c4dabc3bfc96704b1d426a3d
Maxim Sergeev <themaximsergeev@gmail.com>
2020-12-04 11:09:16 +0200

    Controller for sending confirmation emails.

--- back/controllers/emailController.js
+++ back/controllers/emailController.js
@@ -1,15 +1,25 @@
 const emailRouter = require('express').Router()
 
 const sendMail = require('./mailer')
+const termsData = require('../utils/data/terms.json')
 
-emailRouter.post('/', (req, res) => {
-  const {
-    email, confirmation, terms,
-  } = req.body
+function makeTerms(options) {
+  if (options.hsy) return `${termsData[options.distance] + termsData.hsy}\n${termsData.defaultTerms}`
 
+  return `${termsData[options.distance]}\n${termsData.defaultTerms}`
+}
+
+emailRouter.post('/', (req, res, next) => {
+  const { email, confirmation, options } = req.body
   const subject = 'VARAUSVAHVISTUS'
+  const terms = makeTerms(options)
 
-  sendMail(email, subject, `${confirmation}\n\n${terms}`)
+  try {
+    sendMail(email, subject, `${confirmation}\n\n${terms}`)
+  } catch (err) {
+    res.send({ error: err.message })
+    next(err)
+  }
 
   res.status(200).send('Email sent successfully')
 })
