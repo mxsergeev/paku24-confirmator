18a6cc62a9f2f3401f629bd1aae076f3f0c8d6ce
Maxim Sergeev <themaximsergeev@gmail.com>
2020-12-04 11:11:56 +0200

    Leftovers.

--- back/app.js
+++ back/app.js
@@ -1,21 +1,29 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-// const calendarRouter = require('./controllers/calendar')
+
+const calendarRouter = require('./controllers/calendarController')
 const emailRouter = require('./controllers/emailController')
-// const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/loginController')
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
-morgan('dev')
-// app.use('/api/calendar', calendarRouter)
+app.use('/api/calendar', calendarRouter)
 app.use('/api/email', emailRouter)
-// app.use('/api/notes', notesRouter)
+app.use('/api/login', loginRouter)
 
 app.get('/', (req, res) => {
+  console.log('hello')
   res.send('Hello world!')
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 module.exports = app
