02fb5add436d1f01bb8747bffaf4b4cd7026bff4
Maxim Sergeev <themaximsergeev@gmail.com>
2020-12-08 09:46:23 +0200

    Extracted a few not so related functions into their own file.

--- front/src/utils/regexFunctions.js
+++ front/src/utils/regexFunctions.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-control-regex */
 /* eslint-disable no-useless-escape */
 import services from './services.json'
-
-function cannotFind(str) {
-  return `Cannot find ${str}. Invalid format.`
-}
-
-export function toConfirmationDateFormat(date) {
-  return new Date(date).toLocaleDateString().replace(/\.|\//g, '-')
-}
+import * as calculations from './helpers/calculations.js'
+import * as helpers from './helpers/regexHelpers.js'
 
 export function getEventForCalendar(formattedStr) {
   const ev = /(?<=LÄHTÖPAIKKA\n)(.*\s*)*(?=\n\nKIITOS VARAUKSESTANNE!)/.exec(formattedStr)
@@ -19,59 +13,37 @@ export function getEventForCalendar(formattedStr) {
 export function getStartingTime(str) {
   const dateRegex = /(?<=Date end time: \w+, )\w+ \d+(th|rd|st|nd)? \w+/.exec(str)
 
-  if (!dateRegex) throw new Error(cannotFind('date'))
+  if (!dateRegex) throw new Error(helpers.cannotFind('date'))
 
   const original = new Date(dateRegex[0].replace(/th|rd|st|nd/, '') + 'Z')
   const ISODate = original.toISOString().split('T')[0]
-  const confirmationFormat = original.toLocaleDateString().replace(/\.|\//g, '-')
+  const confirmationFormat = helpers.toConfirmationDateFormat(original)
 
   const time = /\d+:\d+/.exec(str)[0]
-  const timeNumber = /\d+/.exec(time)[0]
 
-
-  return { confirmationFormat, original, ISODate, time, timeNumber }
+  return { confirmationFormat, original, ISODate, time }
 }
 
 export function getDuration(str) {
   const duration = /\d+(\.\d+)?(?=\sh.)/.exec(str)
 
-  if(!duration) throw new Error(cannotFind('duration'))
+  if(!duration) throw new Error(helpers.cannotFind('duration'))
 
   return duration[0]
 }
 
 export function getFees(str) {
   const date = getStartingTime(str).original
-
-  const dayOFWeek = date.getDay()
-  const weekEndFee = dayOFWeek === 6 || dayOFWeek === 7 || dayOFWeek === 0 ? 15 : false
-
-  const dayOfMonth = date.getDate()
-  const endOfMonth = [ new Date(date.getFullYear(), date.getMonth()+1, 0).getDate(), 1 ]
-  let endOfMonthFee = endOfMonth.includes(dayOfMonth) ? 15 : false
-
-  if (weekEndFee) endOfMonthFee = false
-
-  const time = getStartingTime(str).timeNumber
-  const morningFee = time < 8 ? 20 : false
-  const nightFee = time > 20 ? 20 : false
-
+  const time = getStartingTime(str).time
   const paymentType = getPaymentType(str)
-  const paymentFee = paymentType === "Lasku" ? 14 : false
 
-  return [ 
-    { value: weekEndFee, name: 'VIIKONLOPPULISÄ' }, 
-    { value: endOfMonthFee, name: 'KUUNVAIHDELISÄ' }, 
-    { value: morningFee, name: 'AAMULISÄ' }, 
-    { value: nightFee, name: 'YÖLISÄ' },
-    { value: paymentFee, name: 'LASKULISÄ' }, 
-  ]
+  return calculations.calculateFees(date, time, paymentType)
 }
 
 export function getService(str) {
   let price = /(?<=PRICE: )\d+/.exec(str)
 
-  if (!price) throw new Error(cannotFind('price'))
+  if (!price) throw new Error(helpers.cannotFind('price'))
   
   price = price[0]
 
@@ -91,27 +63,27 @@ export function getService(str) {
 export function getPaymentType(str) {
   let paymentType = /(?<=Payment Type: )[A-Öa-ö]+/.exec(str)
 
-  if (!paymentType) throw new Error(cannotFind('payment type'))
+  if (!paymentType) throw new Error(helpers.cannotFind('payment type'))
 
   return paymentType[0]
 }
 
-export function getAdress(str, course) {
+export function getAddress(str, course) {
   let city = new RegExp(`(?<=${course}: )[A-Öa-ö]+`).exec(str)
   city = city ? city[0] : ''
 
-  let adress = new RegExp(`(?<=${course}: ${city} / ).*(?=,*)`).exec(str)
-  adress = adress ? `${adress[0]},` : ''
+  let address = new RegExp(`(?<=${course}: ${city} / ).*(?=,*)`).exec(str)
+  address = address ? `${address[0]},` : ''
 
-  if (course === 'Frome' && !adress) throw new Error(cannotFind('adress'))
+  if (course === 'Frome' && !address) throw new Error(helpers.cannotFind('address'))
 
-  return { adress, city }
+  return { address, city }
 }
 
 export function getName(str) {
   const name = /(?<=Name: )([A-Öa-ö]+ *)+/.exec(str)
 
-  if (!name) throw new Error(cannotFind('name'))
+  if (!name) throw new Error(helpers.cannotFind('name'))
 
   return name[0]
 }
@@ -119,7 +91,7 @@ export function getName(str) {
 export function getEmail(str) {
   const email = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.exec(str)
 
-  if (!email) throw new Error(cannotFind('email'))
+  if (!email) throw new Error(helpers.cannotFind('email'))
 
   return email[0]
 }
@@ -127,7 +99,7 @@ export function getEmail(str) {
 export function getPhone(str) {
   const phone = /(?<=Phone: )[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*/.exec(str)
 
-  if (!phone) throw new Error(cannotFind('phone'))
+  if (!phone) throw new Error(helpers.cannotFind('phone'))
 
   return phone[0].replace(/\n/, '')
 }
@@ -144,16 +116,4 @@ export function getComment(str) {
   }
 
   return comment
-}
-
-export function printFee(str) {
-  const activeFees = getFees(str).filter(fee => fee.value)
-
-  const arrayOfFees = activeFees.map(fee => fee.name)
-  
-  const feesInText = activeFees
-    .map(fee => `\n${fee.name}\n${fee.value}€`)
-    .reduce((acc, cur) => acc + cur, '')
-
-  return { array: arrayOfFees, string: feesInText }
 }
