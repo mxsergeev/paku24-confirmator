515f2b9d24f1e0b7bf74535e230b3a1b242cbe98
Maxim Sergeev <themaximsergeev@gmail.com>
2020-12-08 09:47:31 +0200

    Regex helpers.

---
+++ front/src/utils/helpers/calculations.js
@@ -0,0 +1,26 @@
+export function calculateFees(date, time, paymentType) {
+  const timeInNumberType = time.split(':')[0] * 1
+  console.log(date, timeInNumberType, paymentType)
+
+  const dayOFWeek = date.getDay()
+  const weekEndFee = dayOFWeek === 6 || dayOFWeek === 7 || dayOFWeek === 0 ? 15 : false
+
+  const dayOfMonth = date.getDate()
+  const endOfMonth = [ new Date(date.getFullYear(), date.getMonth()+1, 0).getDate(), 1 ]
+  let endOfMonthFee = endOfMonth.includes(dayOfMonth) ? 15 : false
+
+  if (weekEndFee) endOfMonthFee = false
+
+  const morningFee = timeInNumberType < 8 ? 20 : false
+  const nightFee = timeInNumberType > 20 ? 20 : false
+
+  const paymentFee = paymentType === "Lasku" ? 14 : false
+
+  return [ 
+    { value: weekEndFee, name: 'VIIKONLOPPULISÄ' }, 
+    { value: endOfMonthFee, name: 'KUUNVAIHDELISÄ' }, 
+    { value: morningFee, name: 'AAMULISÄ' }, 
+    { value: nightFee, name: 'YÖLISÄ' },
+    { value: paymentFee, name: 'LASKULISÄ' }, 
+  ]
+}
---
+++ front/src/utils/helpers/regexHelpers.js
@@ -0,0 +1,21 @@
+export function cannotFind(str) {
+  return `Cannot find ${str}. Invalid format.`
+}
+
+export function toConfirmationDateFormat(date) {
+  return new Date(date).toLocaleDateString().replace(/\.|\//g, '-')
+}
+
+export function printFees(fees) {
+  const activeFees = fees.filter(fee => fee.value)
+
+  const arrayOfFeeNames = activeFees.map(fee => fee.name)
+  
+  const feesInText = activeFees
+    .map(fee => `\n${fee.name}\n${fee.value}€`)
+    .reduce((acc, cur) => acc + cur, '')
+
+  console.log({ array: arrayOfFeeNames, string: feesInText })
+
+  return { array: arrayOfFeeNames, string: feesInText }
+}
