3d85eb60108944c5ec770096202848f97a756198
Maxim Sergeev <themaximsergeev@gmail.com>
2021-02-24 14:51:01 +0200

    Date in the confirmation is now always in 'dd-mm-yyyy' format regardless of browser.

--- front/src/utils/helpers/regexHelpers.js
+++ front/src/utils/helpers/regexHelpers.js
@@ -3,7 +3,18 @@ export function cannotFind(str) {
 }
 
 export function toConfirmationDateFormat(date) {
-  return new Date(date).toLocaleDateString().replace(/\.|\//g, '-')
+  let dd = date.getDate()
+  let mm = date.getMonth() + 1
+  const yyyy = date.getFullYear()
+
+  if (dd < 10) {
+    dd = `0${dd}`
+  }
+  if (mm < 10) {
+    mm = `0${mm}`
+  }
+
+  return `${dd}-${mm}-${yyyy}`
 }
 
 export function printFees(fees) {
