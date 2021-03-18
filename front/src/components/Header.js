abfbbc465db69bdc60ae28cb32b8ab94528164b6
Maxim Sergeev <themaximsergeev@gmail.com>
2021-03-18 10:34:41 +0200

    Fix: Custom switch is now unchecked when clicking on logo

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -15,7 +15,7 @@ export default function Header({ isLogged, custom, setCustom, handleChange }) {
   return (
     <div className="logo">
       <div>
-        <Link to={isLogged ? '/' : '/login'}>
+        <Link onClick={() => setCustom(false)} to={isLogged ? '/' : '/login'}>
           <img src={logo} alt="Logo" width="125px" />
         </Link>
         <span className="text">CONFIRMATOR</span>
