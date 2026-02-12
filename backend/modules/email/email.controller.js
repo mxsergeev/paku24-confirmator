93758df38eb298d096194f6a6c1f23d0e11a4193
Sofia Savchukova <thesavchukova@gmail.com>
2026-02-12 11:53:02 +0000

    feat: enhance email confirmation handling to support multiple email sources

--- backend/modules/email/email.controller.js
+++ backend/modules/email/email.controller.js
@@ -6,17 +6,21 @@ const authMW = require('../authentication/auth.middleware')
 emailRouter.use(authMW.authenticateAccessToken)
 
 emailRouter.post('/send-confirmation', (req, res, next) => {
-  const { orderDetails, order } = req.body
-  const terms = makeTerms(order)
+  const { orderDetails, order, options, email } = req.body
+
+  const orderForTerms = order || options || {}
+  const targetEmail = (order && order.email) || email || (orderForTerms && orderForTerms.email)
+
+  const terms = makeTerms(orderForTerms)
   const subject = 'VARAUSVAHVISTUS'
   const body = `VARAUSVAHVISTUS\n${orderDetails}\nKIITOS VARAUKSESTANNE!\n\n${terms}`
 
   sendMail({
-    email: order.email,
+    email: targetEmail,
     subject,
     body,
   })
-    .then(() => res.status(200).send({ message: `Email sent to ${order.email}.` }))
+    .then(() => res.status(200).send({ message: `Email sent to ${targetEmail}.` }))
     .catch((err) => next(err))
 })
 
