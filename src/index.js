0aa678d1e42dba9ba800f87b96bded8c6749eafa
Sofia Savchukova <thesavchukova@gmail.com>
2026-03-06 12:51:45 +0000

    feat: add react-query v4 and app-level QueryClientProvider

--- src/index.js
+++ src/index.js
@@ -3,32 +3,46 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import CancelIcon from '@material-ui/icons/Cancel'
 import App from './App'
 import './index.css'
 
 const notistackRef = React.createRef()
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      cacheTime: 1000 * 60 * 10, // 10 minutes
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/app">
-      <SnackbarProvider
-        action={(key) => (
-          <CancelIcon
-            onClick={() => {
-              notistackRef.current.closeSnackbar(key)
-            }}
-          />
-        )}
-        ref={notistackRef}
-        maxSnack={3}
-        dense
-        autoHideDuration={5000}
-        anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
-      />
+    <QueryClientProvider client={queryClient}>
+      <Router basename="/app">
+        <SnackbarProvider
+          action={(key) => (
+            <CancelIcon
+              onClick={() => {
+                notistackRef.current.closeSnackbar(key)
+              }}
+            />
+          )}
+          ref={notistackRef}
+          maxSnack={3}
+          dense
+          autoHideDuration={5000}
+          anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+        />
 
-      <App />
-    </Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
