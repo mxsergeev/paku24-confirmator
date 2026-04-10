429ee8e45a37c86831cd1efbf202a222881a9c2a
Sofia Savchukova <thesavchukova@gmail.com>
2026-04-10 13:30:58 +0000

    feat: integrate email helpers into confirmation endpoint

--- backend/modules/email/email.controller.js
+++ backend/modules/email/email.controller.js
@@ -3,26 +3,31 @@ import express from 'express'
 const emailRouter = express.Router()
 
 import sendMail, { sendMailWithAttachment } from './email.awsAPI.js'
-import { makeTerms } from './email.helpers.js'
+import { buildConfirmationEmail, makeTerms } from './email.helpers.js'
 import dayjs from '../../../src/shared/dayjs.js'
 import * as authMW from '../authentication/auth.middleware.js'
 
 emailRouter.use(authMW.authenticateAccessToken)
 
 emailRouter.post('/send-confirmation', (req, res, next) => {
-  const { orderDetails, order, options, email } = req.body
+  const { orderDetails, order, options, email, lang } = req.body
 
   const orderForTerms = order || options || {}
   const targetEmail = (order && order.email) || email || (orderForTerms && orderForTerms.email)
 
   const terms = makeTerms(orderForTerms)
-  const subject = 'VARAUSVAHVISTUS'
-  const body = `VARAUSVAHVISTUS\n${orderDetails}\nKIITOS VARAUKSESTANNE!\n\n${terms}`
+  const { subject, body } = buildConfirmationEmail({
+    order,
+    orderDetails,
+    terms,
+    lang: lang || order?.lang || order?.locale,
+  })
 
   sendMail({
     email: targetEmail,
     subject,
     body,
+    html: true,
   })
     .then(() => res.status(200).send({ message: `Email sent to ${targetEmail}.` }))
     .catch((err) => next(err))
